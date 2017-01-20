3a95632c6f72840ccb424d400a4a5d8138125113
paddeee <paddeee@googlemail.com>
2017-01-20 12:00:36 +0000

    Slider working nicely

--- source/js/slider.js
+++ source/js/slider.js
@@ -25,7 +25,10 @@ jQuery(document).ready(function ($) {
 		}
 	};
 
+	var slider;
+	var heroSlider;
 	var $slider = document.querySelector('#Slider');
+	var $heroSlider = document.querySelector('#HeroSlider');
 	var $sliderArrowsContainer = document.querySelector('#SliderArrowsContainer');
 
 	// Hide navigation arrows if viewing on touch screen device
@@ -36,13 +39,34 @@ jQuery(document).ready(function ($) {
 
 	if ($slider) {
 
-		var slider = new $JssorSlider$("Slider", sliderOptions);
+		slider = new $JssorSlider$("Slider", sliderOptions);
 
 		function ScaleSlider() {
 			var refSize = slider.$Elmt.parentNode.clientWidth;
 			if (refSize) {
-				refSize = Math.min(refSize, 1920);
+				refSize = Math.min(refSize, 800);
 				slider.$ScaleWidth(refSize);
+			}
+			else {
+				window.setTimeout(ScaleSlider, 30);
+			}
+		}
+		ScaleSlider();
+		$(window).bind("load", ScaleSlider);
+		$(window).bind("resize", ScaleSlider);
+		$(window).bind("orientationchange", ScaleSlider);
+		//responsive code end
+	}
+
+	if ($heroSlider) {
+
+		heroSlider = new $JssorSlider$("HeroSlider", sliderOptions);
+
+		function ScaleSlider() {
+			var refSize = heroSlider.$Elmt.parentNode.clientWidth;
+			if (refSize) {
+				refSize = Math.min(refSize, 1920);
+				heroSlider.$ScaleWidth(refSize);
 			}
 			else {
 				window.setTimeout(ScaleSlider, 30);
