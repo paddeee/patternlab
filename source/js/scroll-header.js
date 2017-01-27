39ee512d4b5557e02e9c96c13e01315aec6cc9a4
paddeee <paddeee@googlemail.com>
2017-01-27 17:47:19 +0000

    Finished homepage

---
+++ source/js/scroll-header.js
@@ -0,0 +1,34 @@
+var scrollAnimatedHeader = (function() {
+
+  var docElem = document.documentElement;
+  var header = document.querySelector('.header');
+  var didScroll = false;
+  var changeHeaderOn = 300;
+
+  function init() {
+    window.addEventListener( 'scroll', function( event ) {
+      if( !didScroll ) {
+        didScroll = true;
+        setTimeout( scrollPage, 250 );
+      }
+    }, false );
+  }
+
+  function scrollPage() {
+    var sy = scrollY();
+    if ( sy >= changeHeaderOn ) {
+      header.classList.add('header--shrink' );
+    }
+    else {
+      header.classList.remove('header--shrink' );
+    }
+    didScroll = false;
+  }
+
+  function scrollY() {
+    return window.pageYOffset || docElem.scrollTop;
+  }
+
+  init();
+
+})();
